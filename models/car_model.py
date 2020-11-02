747dedf48b36dc392b181e0e36ea002d0070e58e
MrMohammadY <m.yazdani000@gmail.com>
2020-11-03 01:24:17 +0330

    add car class and create attribute

---
+++ models/car_model.py
@@ -0,0 +1,7 @@
+class Car:
+
+    def __init__(self, plaque, model='', company='', color=''):
+        self.plaque = plaque
+        self.model = model
+        self.company = company
+        self.color = color
