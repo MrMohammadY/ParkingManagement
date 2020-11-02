3f512674e4709a436af3652dfb63678f4a76c9ce
MrMohammadY <m.yazdani000@gmail.com>
2020-11-03 01:43:05 +0330

    add commend in these files

--- models/car_model.py
+++ models/car_model.py
@@ -1,6 +1,13 @@
 class Car:
 
     def __init__(self, plaque, model='', company='', color=''):
+        """
+        set attribute for cars
+        :param plaque: take plaque
+        :param model: optional | take model car
+        :param company: optional | take company car
+        :param color: optional | take color car
+        """
         self.plaque = plaque
         self.model = model
         self.company = company
--- models/park_model.py
+++ models/park_model.py
@@ -1,8 +1,14 @@
 class Park:
-    parking_counter = 0
+    parking_counter = 0  # for create number of parking and create unit code
 
     def __init__(self, name, address, capacity):
-        self.parking_id = Park.parking_counter + 1
+        """
+        Set attribute for parking
+        :param name: parking name
+        :param address: parking address
+        :param capacity: parking opacity
+        """
+        self.parking_id = Park.parking_counter + 1  # create a unit code for parkings
         self.parking_name = name
         self.parking_address = address
         self.parking_capacity = capacity
@@ -14,9 +20,16 @@ class Park:
         Park.parking_counter += 1
 
     def is_full(self):
+        """
+        Checking that the parking lot is full?
+        :return: True: parking is full || False: parking is free
+        """
         return all(status[0] is False for status in self.park_place.values())
 
     def report_free_park_place(self):
+        """
+        :return: list of free park place
+        """
         list_of_free_park_place = list()
 
         for key, val in self.park_place.items():
@@ -26,6 +39,9 @@ class Park:
         return list_of_free_park_place
 
     def report_busy_park_place(self):
+        """
+        :return: list of busy park place
+        """
         list_of_busy_park_place = list()
 
         for key, val in self.park_place.items():
@@ -36,6 +52,12 @@ class Park:
 
     def show_information_about_busy_park_place(self, list_of_busy_park_place,
                                                number_busy_park_place):
+        """
+        take number of place for check car plaque
+        :param list_of_busy_park_place: take list of busy place
+        :param number_busy_park_place: take number of place for check
+        :return:
+        """
         if len(list_of_busy_park_place) == 0:
             print('All place is free!')
 
@@ -43,9 +65,14 @@ class Park:
             info = self.park_place[number_busy_park_place]
             print(f'this plaque: {info[1]}\n for'
                   f'this place {number_busy_park_place}')
+
         else:
             print('This position is free!')
 
     def find_free_park_place(self, list_of_free_park_place):
+        """
+        take list of free place and return first free place to Factor
+        :param list_of_free_park_place: take list of free place
+        :return: number of free place
+        """
         return list_of_free_park_place[0]
-
