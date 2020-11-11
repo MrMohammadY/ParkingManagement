8b5c8ebe6016819d456e35e4d7245be9e43be49d
HosseinRanjbari <ranjbari.hossein9945@gmail.com>
2020-11-11 21:37:26 +0330

    some efficiency on its motheds

--- mongodb_connection.py
+++ mongodb_connection.py
@@ -1,49 +1,99 @@
 import pymongo
-from config import password
+from config import mongodb_password
 
-client = pymongo.MongoClient(f"mongodb+srv://MMD:{password}"
-                             f"@parkingmanagementcluste.kihxb.mongodb.net/"
+# connection with mongodb and save our link into client
+# client = pymongo.MongoClient(f"mongodb+srv://MMD:{mongodb_password}"
+#                              f"@parkingmanagementcluste.kihxb.mongodb.net/"
+#                              f"<dbname>?retryWrites=true&w=majority")
+
+client = pymongo.MongoClient(f"mongodb+srv://Hossein:{mongodb_password}"
+                             f"@cluster0.y9iwi.mongodb.net/"
                              f"<dbname>?retryWrites=true&w=majority")
+
+# connect to our parking users database.
+
+parking_users_db = client.Parking_Users
+
+# choose Users collection.
+users_collection = parking_users_db['Users']
+
+
+def is_this_user_exist(username, password):
+    """
+    check and authentication this user exist in our system or not base on his username and password.
+    :param username: parking username
+    :param password: parking password
+    :return: True if this user exist in our system and
+             False if this user doesn't exist.
+    """
+
+    # find out this user
+    user = users_collection.find_one({'username': username, 'password': password})
+
+    # check user exist or not
+    if user is not None:
+        return True
+    return False
 
 
 def connect_to_specific_database(username):
+    """
+    connect to a specific parking database base on its username.
+    :param username: parking username.
+    :return: a particular parking database.
+    """
+
     db = client[f'{username}']
-    collection = db[f'{username}_DATA']
-    return collection.find_one()
+    return db
 
 
-def log_in_check_exist_username(username, pass_word):
-    db = client.Parking_Users
-    collection = db['Users']
-    users = collection.find_one({'username': username, 'password': pass_word})
-    if users is not None:
-        return connect_to_specific_database(username)
-    else:
-        print('Your Username Or Password Incorrect!')
-        return False
+def sign_up_users(username, password, confirm_password):
+    """
+    a method for check user sign up process and add this user to our system.
+    :param username: new parking username
+    :param password: new parking password
+    :param confirm_password: new parking confirm password.
+    :return: True if this process completely done without any problems and
+             False if some problems occur in this process.
+    """
 
+    # by default we considering this user doesn't created and
+    # if user created change this situation.
+    can_create_new_user = False
 
-def sing_up_check_users(username, pass_word, confirm_password):
-    db = client.Parking_Users
-    collection = db['Users']
-    users = collection.find()
-    list_users = [u['username'] for u in users]
+    # check this username already exist or not
+    if not(is_this_user_exist(username, password)):
 
-    if username not in list_users:
-        if pass_word == confirm_password:
-            data = {'username': username, 'password': pass_word}
-            collection.insert_one(data)
-            print('Create User')
-            return True
+        # check user password and confirm password are same.
+        if password == confirm_password:
+
+            # if everything is ok, add this user to our system.
+            user_data = {'username': username, 'password': password}
+            users_collection.insert_one(user_data)
+
+            print('@' * 60)
+            print('Create This User')
+            print('@' * 60)
+
+            # change default situation.
+            can_create_new_user = True
+            return can_create_new_user
+
         else:
-            print('Password is not correct!')
-            return False
+            # if password and confirm password aren't same.
+            print('@' * 60)
+            print('password and confirm password are not same!')
+            print('@' * 60)
+            return can_create_new_user
     else:
-        print('Username is exist!')
-        return False
+        # if this username already exist.
+        print('@' * 60)
+        print('This username is already exist and taken by another users!')
+        print('@' * 60)
+        return can_create_new_user
 
 
-def create_database_user(username, data):
-    new_db = client[f'{username}']
-    collection = new_db[f'{username}_DATA']
-    collection.insert_one(data)
+def create_particular_database_for_each_parking(username, data):
+    new_parking_db = client[f'{username}']
+    new_parking_collection = new_parking_db[f'{username}_DATA']
+    new_parking_collection.insert_one(data)
